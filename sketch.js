38a1547583c7d9a1afcaa5442531a33293343d69
thomaskno <tk2325@nyu.edu>
2019-10-16 16:12:37 -0400

    fractal added

--- sketch.js
+++ sketch.js
@@ -1,6 +1,12 @@
 function setup() {
   // put setup code here
   createCanvas(windowWidth,windowHeight);
+  fill(0,0,0,0);
+	translate(width/2, height/2);
+  fractal(400);
+  translate(0, 0);
+
+  
 }
 
 function draw() {
@@ -8,4 +14,19 @@ function draw() {
 
   fill(0,255,0,100);
   rect(10,50,300,100);
+
+
+
+
+}
+
+function fractal(iter){
+	if(iter == 0){
+		 
+	}else{
+		ellipse(0, 0,iter*2,iter*.9);
+		rotate(5);
+		iter = iter - 1;
+		fractal(iter);
+	}
 }
