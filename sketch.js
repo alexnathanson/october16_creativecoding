9e7aadacec3e72ab890857de2bbf89a0635fbb29
alexnathanson <alex@alexnathanson.com>
2019-10-16 15:41:14 -0400

    alex

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
 function setup() {
   // put setup code here
+  createCanvas(windowWidth,windowHeight);
 }
 
 function draw() {
   // put drawing code here
+
+  fill(0,255,0,100);
+  rect(10,50,300,100);
 }
